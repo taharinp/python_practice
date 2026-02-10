af852ce7a14c25ab69824ba4e9f68704a1592b1f
taharinp <pateltaharin@gmail.com>
2026-02-10 12:13:19 +0530

    Add files

---
+++ section21_unicode.py
@@ -0,0 +1,8 @@
+Python 3.12.8 (tags/v3.12.8:2dc476b, Dec  3 2024, 19:30:04) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> s='\u03b1'
+>>> s
+'α'
+>>> s='\u03b1\u03b2\u03b3'
+>>> s
+'αβγ'
